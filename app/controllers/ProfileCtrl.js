67532d42c979a722755151c3e7a87ce6eded7f46
Harper Frankstone <Harper Frankstone>
2017-03-20 13:59:58 -0500

    still working with markers and popups, edited the products.json to include addresses

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -13,37 +13,57 @@ app.controller('ProfileCtrl', function($scope, SearchTermData, ProductFactory, A
 		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw', {
 		    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		    maxZoom: 18,
-		    id: 'your.mapbox.project.id',
+		    id: 'hfrankst.4a5bytnl',
 		    accessToken: 'pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw'
 		}).addTo(mymap);
 
-		var marker = L.marker([36.1325, -86.7566]).addTo(mymap);
+		// var marker = L.marker([36.1325, -86.7566]).addTo(mymap);
 	};
 	leaflet();
 
 	$scope.onEachFeature = (feature, layer) => {
-		console.log("the 'Map Promo' button has been pressed");
 		console.log("the store name", feature);
+		console.log("feature.store", feature.store);
 
 		//don't think this is the best way to do this but fuck it
-		if(feature === "Kroger"){
+		if(feature.store === "Kroger"){
 			var kroger = L.marker([36.1199, -86.7775]).addTo(mymap);
-		} else if (feature === "ALDI") {
+		} else if (feature.store === "ALDI") {
 			var aldi = L.marker([36.0903, -86.7323]).addTo(mymap);
-		} else if (feature === "Publix") {
+		} else if (feature.store === "Publix") {
 			var publix = L.marker([36.1266, -86.8474]).addTo(mymap);
 		}
+		 if (feature.properties && feature.properties.popupContent) {
+			        layer.bindPopup(feature.properties.popupContent);
+			    }
 
-		// mapboxgl.accessToken = 'pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw'; // replace this with your access token
-		    // var map = new mapboxgl.Map({
-		    //   container: 'mapid',
-		    //   // style: '{style URL}' // replace this with your style
-		    // });
+		// mapboxgl.accessToken = 'pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw'; 
+		//    var map = new mapboxgl.Map({
+		//      container: 'mapid',
+		//      style: 'mapbox://styles/hfrankst/cj0icofq9005b2sqprh6degmc' 
+		//    });
+		//    // code from the next step will go here
+
+		//    map.on('click', function(e) {
+		//      var features = map.queryRenderedFeatures(e.point, {
+		//        layers: ['streets'] 
+		//      });
+
+		//      if (!features.length) {
+		//        return;
+		//      }
+
+		//      var feature = features[0];
+
+		//      var popup = new mapboxgl.Popup({ offset: [0, -15] })
+		//        .setLngLat(feature.geometry.coordinates)
+		//        .setHTML('<h3>' + feature.properties.name + '</h3><p>' + feature.properties.address + '</p><p>' + feature.properties.phone + '</p>' )
+		//        .setLngLat(feature.geometry.coordinates)
+		//        .addTo(map);
+		//    });
 
 
-		// if (feature.properties && feature.properties.popupContent) {
-	 //        layer.Popup(feature.properties.popupContent);
-	 //    }
+		//
 		// console.log("looking into the feature", feature.properties);
 
 		//this code is taking from a mapbox tutorial 
