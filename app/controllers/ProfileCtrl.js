1f1da03ef93eeed438a6e80b51c11a3d5ebcae8f
Harper Frankstone <Harper Frankstone>
2017-03-20 11:51:31 -0500

    sloppy looking code, going to clean that up and I had to switch the order of coordinates in my geojson. I think that was giving me trouble

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -7,9 +7,6 @@ app.controller('ProfileCtrl', function($scope, SearchTermData, ProductFactory, A
 	//build an object with the long and lat of each store hard coded, so you can call on them in the leaflet function
 	//I may need to build a function that takes the store front name from the info that is in the ng-repeat and then correctly feed that store's coords to the leaflet function
 	var mymap = L.map('mapid').setView([36.1325, -86.7566], 15);
-	var kroger = L.marker([36.1199, -86.7775]).addTo(mymap);
-	var aldi = L.marker([36.0903, -86.7323]).addTo(mymap);
-	var publix = L.marker([36.1266, -86.8474]).addTo(mymap);
 
 	let leaflet = () => {
 
@@ -26,34 +23,65 @@ app.controller('ProfileCtrl', function($scope, SearchTermData, ProductFactory, A
 
 	$scope.onEachFeature = (feature, layer) => {
 		console.log("the 'Map Promo' button has been pressed");
-	    // does this feature have a property named popupContent?
-	    if (feature.properties && feature.properties.popupContent) {
-	        layer.bindPopup(feature.properties.popupContent);
-	    }
-	
+		console.log("the store name", feature);
 
-		var geojsonFeature = 
-		{
-		    "type": "Feature",
-		    "geometry": {
-		        "type": "Point",
-		        "coordinates": [36.1199, 86.7775]
-		    },
-		    "properties": {
-		        "name": "Kroger",
-		        "amenity": "Grocery Store",
-		        "popupContent": "This is where the promo is offered!"
-		    }
-		};
-		// console.log("geojsonFeature", geojsonFeature.geometry.coordinates);
-		// var kroger = geojsonFeature.geometry.coordinates;
+		//don't think this is the best way to do this but fuck it
+		if(feature === "Kroger"){
+			var kroger = L.marker([36.1199, -86.7775]).addTo(mymap);
+		} else if (feature === "ALDI") {
+			var aldi = L.marker([36.0903, -86.7323]).addTo(mymap);
+		} else if (feature === "Publix") {
+			var publix = L.marker([36.1266, -86.8474]).addTo(mymap);
+		}
 
-		new L.GeoJSON(geojsonFeature).addTo(mymap);
-		console.log("geoJson test", L.GeoJSON(geojsonFeature).addTo(mymap));
+		// mapboxgl.accessToken = 'pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw'; // replace this with your access token
+		    // var map = new mapboxgl.Map({
+		    //   container: 'mapid',
+		    //   // style: '{style URL}' // replace this with your style
+		    // });
+
+
+		// if (feature.properties && feature.properties.popupContent) {
+	 //        layer.Popup(feature.properties.popupContent);
+	 //    }
+		// console.log("looking into the feature", feature.properties);
+
+		//this code is taking from a mapbox tutorial 
+		 // map.on('click', function(e) {
+		   // var features = map.queryRenderedFeatures(e.point, {
+		   //   layers: ['{layer name}'] // replace this with the name of the layer
+		   // });
+
+		   // if (!features.length) {
+		   //   return;
+		   // }
+
+		 //   var feature = 
+		 //   		{
+		 //   		    "type": "Feature",
+		 //   		    "geometry": {
+		 //   		        "type": "Point",
+		 //   		        "coordinates": [36.1199, -86.7775]
+		 //   		    },
+		 //   		    "properties": {
+		 //   		        "name": "Kroger",
+		 //   		        "amenity": "Grocery Store",
+		 //   		        "popupContent": "This is where the promo is offered!"
+		 //   		    }
+		 //   		};
+
+		 //   var popup = new mapboxgl.Popup({ offset: [0, -15] })
+		 //     .setLngLat(feature.geometry.coordinates)
+		 //     .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+		 //     .setLngLat(feature.geometry.coordinates)
+		 //     .addTo(mymap);
+		 // });
+
+
+		// new L.GeoJSON(feature).addTo(mymap);
 	};
 
 
-	//I want this function to fire once the '/profile' partial loads
 	let userPromos = () => {
 		ProductFactory.getUsersPromos(user)
 		.then((userSavedDeals) => {
--- app/values/mapapi.js
+++ app/values/mapapi.js
@@ -3,3 +3,5 @@
 app.constant("MapApi", {
 	mapKey: "AIzaSyAEj6YaqTxOsHaT3Yj3UJHCfCj-cDl38Zk"
 });
+
+// pk.eyJ1IjoiaGZyYW5rc3QiLCJhIjoiY2owY3QzODNpMDUxMDMybGMydnBxeGdncCJ9.NQrhhmJVuy175EbL-IsbJw
