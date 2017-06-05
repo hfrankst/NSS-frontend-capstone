ed97dbf07106a4de0bc6674f455bc11b6d2b9bb3
hfrankst <harperfrankstone@gmail.com>
2017-06-05 15:41:50 -0500

    successfully scraped the webpage with the weekly ads, now just need to get more specific in targetting the right info

--- scrape/kroger.py
+++ scrape/kroger.py
@@ -1,17 +1,30 @@
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 
 
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
 
-url = ("https://www.kroger.com/weeklyad?StoreCode=00542&DivisionId=029", "window['hostedStack'] = ")
+url = "http://www.kroger.com/weeklyad?StoreCode=00542&DivisionId=029"
+
+try:
+    request = requests.get(url, headers=headers, timeout=5)
+    print(request.content)
+except requests.exceptions.Timeout:
+    print("Timeout occurred")
+
+
+
+# url = requests.get("https://www.kroger.com/weeklyad?StoreCode=00542&DivisionId=029")
 # print(url)
 
 # page = urllib.request.urlopen(url)
 # print(page)
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-   html = response.read()
-   print(html)
+# req = urllib.request.Request(url)
+# with urllib.request.urlopen(req) as response:
+#    html = response.read()
+#    print(html)
 
 # soup = BeautifulSoup(page)
 # print(soup) 
